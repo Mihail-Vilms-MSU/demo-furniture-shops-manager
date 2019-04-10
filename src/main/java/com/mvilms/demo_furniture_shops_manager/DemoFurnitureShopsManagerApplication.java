ec24c6c3803575bda09fbe0a7ab356bed4db1af1
Unknown <mihail.vilms@gmail.com>
2019-04-10 16:08:06 +0300

    Bare bones Spring app created

---
+++ src/main/java/com/mvilms/demo_furniture_shops_manager/DemoFurnitureShopsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mvilms.demo_furniture_shops_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFurnitureShopsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFurnitureShopsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mvilms/demo_furniture_shops_manager/DemoFurnitureShopsManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mvilms.demo_furniture_shops_manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoFurnitureShopsManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
